b89d08e18184f270f9bfb2dd9b4739b95b55d827
HUNTER <xujanov.xujamurod@gmail.com>
2025-03-27 17:29:58 +0500

    27-03-2025

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import {useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
+// import BaseComponent from 'bootstrap/js/dist/base-component';
 
-const withSlider = (BaseComponent, getData) => {
+ const withSlider = (BaseComponent, getData) => {
     return (props) => {
         const [slide, setSlide] = useState(0);
         const [autoplay, setAutoplay] = useState(false)
-
+            
         useEffect(() => {
             setSlide(getData());
         }, [])
-
+    
         function changeSlide(i) {
             setSlide(slide => slide + i);
         }
 
-        return <BaseComponent
-            {...props}
-            slide={slide}
-            autoplay={autoplay}
-            changeSlide={changeSlide}
-            setAutoplay={setAutoplay}
-        />
+        return <BaseComponent 
+                {...props}
+                slide={slide} 
+                autoplay={autoplay} 
+                changeSlide={changeSlide} 
+                setAutoplay={setAutoplay}/>
     }
-}
+ }
 
-const getDataFromFirstFetch = () => { return 10 };
-const getDataFromSecondFetch = () => { return 20 };
+const getDataFromFirstFetch = () => {return 10};
+const getDataFromSecondFetch = () => {return 20};
 
 const SliderFirst = (props) => {
 
@@ -37,10 +37,10 @@ const SliderFirst = (props) => {
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
                 <div className="text-center mt-5">Active slide {props.slide}</div>
                 <div className="buttons mt-3">
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => props.changeSlide(-1)}>-1</button>
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => props.changeSlide(1)}>+1</button>
                 </div>
@@ -56,33 +56,32 @@ const SliderSecond = (props) => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {props.slide} <br />{props.autoplay ? 'auto' : null} </div>
+                <div className="text-center mt-5">Active slide {props.slide} <br/>{props.autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => props.changeSlide(-1)}>-1</button>
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => props.changeSlide(1)}>+1</button>
-                    <button
+                    <button 
                         className="btn btn-primary me-2"
                         onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
                 </div>
             </div>
         </Container>
     )
-}
+};
 
 const SliderWithFirstFetch = withSlider(SliderFirst, getDataFromFirstFetch);
 const SliderWithSecondFetch = withSlider(SliderSecond, getDataFromSecondFetch);
 
 const withLogger = WrappedComponent => props => {
     useEffect(() => {
-        console.log('001');
-        
+        console.log('first render!');
     }, []);
-
-    return <WrappedComponent {...props}/>
+    
+    return <WrappedComponent {...props} />
 }
 
 const Hello = () => {
@@ -92,14 +91,13 @@ const Hello = () => {
 }
 
 const HelloWithLogger = withLogger(Hello);
-
+ 
 function App() {
     return (
         <>
-
-            <HelloWithLogger />
-            <SliderWithFirstFetch />
-            <SliderWithSecondFetch />
+            <HelloWithLogger/>
+            <SliderWithFirstFetch/>
+            <SliderWithSecondFetch/>
         </>
     );
 }
